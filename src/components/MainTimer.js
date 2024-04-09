fb49dcefe4d4d3152aa462a692f0de6f3ef03f37
Maria Fredriksson <mf223wk@student.lnu.se>
2024-04-09 10:28:42 +0200

    Add a working timer for seconds

---
+++ src/components/MainTimer.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+
+const MainTimer = () => {
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    // Create a variable to store the interval ID, which will be used to clear the interval later
+    // and hence stop the timer
+    let intervalID = null
+
+    if (isActive && seconds > 0) {
+      intervalID = setInterval(() => {
+        setSeconds((seconds) => seconds - 1)
+      }, 1000)
+    } else if (seconds <= 0) {
+      // Stop the interval when the timer reaches 0 and also set the isActive state to false
+      clearInterval(intervalID) 
+      setIsActive(false)
+    }
+
+    // Clean-up function that first cleans up the previous effect before applying the next effect
+    // and also prevents eventual side effects when the component is unmounted from the DOM
+    return () => clearInterval(intervalID)
+
+    // When the isActive or seconds state changes, the useEffect function will run again
+  }, [isActive, seconds])
+
+  const toggle = () => {
+    if (seconds > 0) {
+      setIsActive(!isActive)
+    } else {
+      // Prevent the timer from starting if seconds are 0
+      setIsActive(false)
+    }
+  }
+
+  const reset = () => {
+    setSeconds(0)
+    setIsActive(false)
+  }
+
+  const handleChange = (e) => {
+    setSeconds(e.target.value)
+  }
+
+  return (
+    <div>
+      <input type="number" value={seconds} onChange={handleChange} />
+      <button onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
+      <button onClick={reset}>Reset</button>
+      <h2>{seconds}s</h2>
+    </div>
+  )
+}
+
+export default MainTimer
