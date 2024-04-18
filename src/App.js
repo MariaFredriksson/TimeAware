374d03aba5e65bdc75827c56f3029df487744c96
Maria Fredriksson <mf223wk@student.lnu.se>
2024-04-18 15:55:40 +0200

    Can add several main timers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 import MainTimer from './components/MainTimer'
 
 function App() {
+  const [timers, setTimers] = useState([])
+
+  const addTimer = () => {
+    setTimers(timers => [...timers, <MainTimer key={timers.length} />])
+  }
+
   return (
     <div className="App">
+      {/* Always show the first timer */}
       <MainTimer />
+
+      {/* Then show additional timers, if there are any */}
+      {timers.map((timer, index) => (
+        <div key={index}>
+          {timer}
+        </div>
+      ))}
+      <br />
+      <button onClick={addTimer}>Add Timer</button>
     </div>
   )
 }
